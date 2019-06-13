811fd93a57eb50c3065c643699b05c8372404522
NikolNiejadlik <niejadlik.nikol@gmail.com>
2019-06-13 10:46:38 +0200

    init commit

---
+++ Odrabianie/Odrabianie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odrabianie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
